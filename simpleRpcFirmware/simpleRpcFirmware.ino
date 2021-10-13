86544ac61a05c6493322a17fe9d2bd48cf6e76c8
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2021-10-13 21:49:42 +0200

    Refactor state decoding

--- simpleRpcFirmware/simpleRpcFirmware.ino
+++ simpleRpcFirmware/simpleRpcFirmware.ino
@@ -3,7 +3,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_FreeTouch.h>
 
-StaticJsonDocument<256> boardState;
+
 Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUM_NEOPIXEL, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 Adafruit_FreeTouch touch = Adafruit_FreeTouch(4, OVERSAMPLE_4, RESISTOR_50K, FREQ_MODE_NONE);
 
@@ -16,7 +16,10 @@ void setColor(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) {
 
 
 void updateBoardState() {
-  boardState["params"][0] = touch.measure() >> 8;
+  StaticJsonDocument<256> boardState;  
+  boardState["jsonrpc"] = "2.0";
+  boardState["method"] = "updateBoardState";
+  boardState["params"][0] = touch.measure() >> 2;
   serializeJson(boardState, Serial);
   Serial.println();
 }
@@ -26,9 +29,6 @@ void setup() {
   Serial.begin(9600);
   pixel.begin();
   touch.begin();
-  
-  boardState["jsonrpc"] = "2.0";
-  boardState["method"] = "updateBoardState";
 }
 
 
