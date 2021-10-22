e9ea2046a8d8e4625a09a43de25fcc2ef656979b
SoerenSofke <16596310+SoerenSofke@users.noreply.github.com>
2021-10-22 21:36:27 +0200

    Port to QT Py

--- simpleRpcFirmware/simpleRpcFirmware.ino
+++ simpleRpcFirmware/simpleRpcFirmware.ino
@@ -1,16 +1,18 @@
-#include <simpleRPC.h>
 #include <ArduinoJson.h>
 #include <Adafruit_NeoPixel.h>
-#include <Adafruit_FreeTouch.h>
 #include <RotaryEncoder.h>
 
-Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUM_NEOPIXEL, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
-Adafruit_FreeTouch touch = Adafruit_FreeTouch(4, OVERSAMPLE_4, RESISTOR_50K, FREQ_MODE_NONE);
-RotaryEncoder encoder(PIN_ENCODER_A, PIN_ENCODER_B, RotaryEncoder::LatchMode::FOUR3);
 
-void checkPosition() {
-  encoder.tick(); // just call tick() to check the state.
-}
+#define NUM_NEOPIXEL 1
+#define PIN_GND 2
+#define PIN_ENCODER_SWITCH 10
+#define PIN_ENCODER_A 1
+#define PIN_ENCODER_B 3
+
+
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUM_NEOPIXEL, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+RotaryEncoder encoder(PIN_ENCODER_A, PIN_ENCODER_B, RotaryEncoder::LatchMode::FOUR0);
+
 
 void setColor(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) {
   pixel.setPixelColor(NUM_NEOPIXEL - 1, red, green, blue);
@@ -18,21 +20,25 @@ void setColor(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) {
   pixel.show();
 }
 
-
 void publishInputStates() {
   StaticJsonDocument<1024> inputStates;
-  inputStates["touch"][0] = touch.measure() >> 2;
 
   static int lastDirection = 0;
   const int currentDirection = (int)encoder.getDirection();
   if (currentDirection != 0) lastDirection = currentDirection;
-  
+
+  switch (lastDirection) {
+    case -1:
+      inputStates["rotary"][1] = "ccw";
+      break;
+    case 1:
+      inputStates["rotary"][1] = "cw";
+      break;
+  }
 
   inputStates["rotary"][0] = encoder.getPosition();
-  inputStates["rotary"][1] = lastDirection;
-  inputStates["rotary"][2] = digitalRead(PIN_ENCODER_SWITCH);
-  
-  
+  inputStates["rotary"][2] = !digitalRead(PIN_ENCODER_SWITCH);
+
   serializeJson(inputStates, Serial);
   Serial.println();
 }
@@ -40,10 +46,9 @@ void publishInputStates() {
 
 void discardControlCharacters() {
   while (isControl(Serial.peek())) {
-      Serial.read();
-    }
+    Serial.read();
+  }
 }
-
 
 
 void updateOutputStates() {
@@ -72,28 +77,36 @@ void updateOutputStates() {
 bool isPeriodPassed(const long period_ms) {
   static unsigned long previousMillis = 0;
   const unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= period_ms) {    
+  if (currentMillis - previousMillis >= period_ms) {
     previousMillis = currentMillis;
     return true;
-   } else {
+  } else {
     return false;
-   }
+  }
+}
+
+
+void checkPosition() {
+  encoder.tick();
 }
 
 
 void setup() {
+  pinMode(PIN_GND, OUTPUT);
+  digitalWrite(PIN_GND, LOW);
+
+  pinMode(PIN_ENCODER_SWITCH, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_A), checkPosition, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_B), checkPosition, CHANGE);
+
+
   Serial.begin(9600);
   Serial.setTimeout(1000);
   pixel.begin();
-  touch.begin();
-
-  attachInterrupt(PIN_ENCODER_A, checkPosition, CHANGE);
-  attachInterrupt(PIN_ENCODER_B, checkPosition, CHANGE);  
-  pinMode(PIN_ENCODER_SWITCH, INPUT_PULLDOWN);
 }
 
 
 void loop() {
   updateOutputStates();
-  if (isPeriodPassed(200)) publishInputStates();
+  if (isPeriodPassed(16)) publishInputStates();
 }
